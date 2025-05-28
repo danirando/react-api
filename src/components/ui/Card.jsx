45e1a2b9ab03a421bfd01e235909e60e512ed11b
danirando <danielerando@tiscali.it>
2025-05-28 14:01:44 +0200

    fix img size

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ actors }) {
   return actors.map((actor) => (
     <div className="col-4" key={actor.id}>
       <div className="card h-100">
-        <img src={actor.image} className="card-img-top h-100" alt="..." />
+        <img src={actor.image} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{actor.name}</h5>
           <p className="card-text">{actor.biography}</p>
@@ -16,14 +16,6 @@ export default function Card({ actors }) {
             Riconoscimenti: {actor.awards.join(", ")}
           </li>
         </ul>
-        <div className="card-body">
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
-        </div>
       </div>
     </div>
   ));
