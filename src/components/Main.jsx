28f39ac0440a4d4d64775ccf1c4e41cbfbb5a545
danirando <danielerando@tiscali.it>
2025-05-28 16:42:12 +0200

    add input filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,19 @@ import ActressCard from "./ui/ActressCard";
 export default function Main() {
   const [actors, setActors] = useState([]);
   const [actresses, setActresses] = useState([]);
+  const [name, SetName] = useState("");
+
+  // Per ogni attore nell'array 'actors', controllo se il suo nome contiene la parola cercata nell'input
+  // Converte entrambi in minuscolo per non fare distinzione tra maiuscole e minuscole
+  // Se sì, lo include nell'array filtrato 'filteredActors'
+  // Stessa cosa per l'array 'actresses'
+  const filteredActors = actors.filter((actor) =>
+    actor.name.toLowerCase().includes(name.toLowerCase())
+  );
+
+  const filteredActresses = actresses.filter((actress) =>
+    actress.name.toLowerCase().includes(name.toLowerCase())
+  );
 
   const fetchActress = (
     url = "https://lanciweb.github.io/demo/api/actresses/"
@@ -26,11 +39,20 @@ export default function Main() {
   return (
     <main>
       <div className="container">
+        <input
+          className="form-control"
+          type="text"
+          value={name}
+          placeholder="Cerca..."
+          name="filter"
+          id="search"
+          onChange={(e) => SetName(e.target.value)}
+        />
         <h1>Actors Cards</h1>
         <div className="row g-3">
-          <ActorsCard actors={actors} />
+          <ActorsCard actors={filteredActors} />
           <h1>Actresses Cards</h1>
-          <ActressCard actresses={actresses} />
+          <ActressCard actresses={filteredActresses} />
         </div>
       </div>
     </main>
--- src/components/ui/ActorsCard.jsx
+++ src/components/ui/ActorsCard.jsx
@@ -1,22 +1,32 @@
 export default function ActorsCard({ actors }) {
-  return actors.map((actor) => (
-    <div className="col-4" key={actor.id}>
-      <div className="card h-100">
-        <img src={actor.image} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{actor.name}</h5>
-          <p className="card-text">{actor.biography}</p>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            Anno di nascita: {actor.birth_year}
-          </li>
-          <li className="list-group-item">Nazionalita: {actor.nationality}</li>
-          <li className="list-group-item">
-            Riconoscimenti: {actor.awards.join(", ")}
-          </li>
-        </ul>
-      </div>
-    </div>
-  ));
+  return (
+    <>
+      {actors.length > 0 ? (
+        actors.map((actor) => (
+          <div className="col-4" key={actor.id}>
+            <div className="card h-100">
+              <img src={actor.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{actor.name}</h5>
+                <p className="card-text">{actor.biography}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  Anno di nascita: {actor.birth_year}
+                </li>
+                <li className="list-group-item">
+                  Nazionalita: {actor.nationality}
+                </li>
+                <li className="list-group-item">
+                  Riconoscimenti: {actor.awards.join(", ")}
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h3>Nessun Risultato</h3>
+      )}
+    </>
+  );
 }
