abc41f040744e90d4cd95e70c7af87b8ed1ad255
danirando <danielerando@tiscali.it>
2025-05-28 13:30:21 +0200

    add axios call to api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,14 @@
 import axios from "axios";
-
+import { useEffect, useState } from "react";
 export default function Main() {
-  return <main></main>;
+  const [actors, setActors] = useState([]);
+
+  const fetchActors = (url = "https://lanciweb.github.io/demo/api/actors/") => {
+    axios.get(url).then((res) => {
+      const results = res.data;
+      setActors(results);
+    });
+  };
+  useEffect(() => fetchActors(), []);
+  return <main>{/* <h1>{actors.map((actor) => actor.name)}</h1> */}</main>;
 }
