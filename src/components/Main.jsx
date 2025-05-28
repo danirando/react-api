aa8b61e5425ec9c3dacfa74da79c45991cd11fe4
danirando <danielerando@tiscali.it>
2025-05-28 13:48:05 +0200

    add cards actors

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,5 +10,47 @@ export default function Main() {
     });
   };
   useEffect(() => fetchActors(), []);
-  return <main>{/* <h1>{actors.map((actor) => actor.name)}</h1> */}</main>;
+  return (
+    <main>
+      <div className="container">
+        <h1>Actors Cards</h1>
+        <div className="row g-3">
+          {actors.map((actor) => (
+            <div className="col-4">
+              <div className="card h-100">
+                <img
+                  src={actor.image}
+                  className="card-img-top h-100"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{actor.name}</h5>
+                  <p className="card-text">{actor.biography}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    Anno di nascita: {actor.birth_year}
+                  </li>
+                  <li className="list-group-item">
+                    Nazionalita: {actor.nationality}
+                  </li>
+                  <li className="list-group-item">
+                    Riconoscimenti: {actor.awards.join(", ")}
+                  </li>
+                </ul>
+                <div className="card-body">
+                  <a href="#" className="card-link">
+                    Card link
+                  </a>
+                  <a href="#" className="card-link">
+                    Another link
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
